bc68ceb424125ba9220f51611d5fac0fc185ebff
David Adams <dladams@bnl.gov>
2015-07-07 16:26:09 -0400

    Add vertex position options.

--- src/g2hepmc.cxx
+++ src/g2hepmc.cxx
@@ -4,9 +4,10 @@
 // For the latter, see e.g.
 // http://http://cepa.fnal.gov/psm/simulation/mcgen/lund/pythia_manual/pythia6.3/pythia6301/node39.html
 
+#include <string>
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <sstream>
 #include "TFile.h"
 #include "TTree.h"
 #include "EVGCore/EventRecord.h"
@@ -19,6 +20,7 @@ using std::string;
 using std::cout;
 using std::endl;
 using std::ofstream;
+using std::istringstream;
 using genie::EventRecord;
 using genie::NtpMCTreeHeader;
 using genie::NtpMCEventRecord;
@@ -34,14 +36,59 @@ int main(int argc, char** argv) {
   cout << "  Input file: " << argv[1] << endl;
   cout << " Output file: " << argv[2] << endl;
 
+  // Open the input file.
   TFile* pfile = TFile::Open(argv[1], "R");
   if ( pfile == nullptr || !pfile->IsOpen() ) {
     cout << "Unable to open input file: " << argv[1] << endl;
     return 2;
   }
 
+  // Open the output file.
   ofstream fout(argv[2], ofstream::out|ofstream::trunc);
   fout.precision(9);
+
+  // Read arguments of form NAME=VAL.
+  double x0 = 0.0;  // Units are mm.
+  double y0 = 0.0;
+  double z0 = 0.0;
+  double t0 = 0.0;  // Units are mm/c
+  int posopt = 1;  // 0=particle, 1=vertex, 2=command line
+  for ( int iarg=3; iarg<argc; ++iarg ) {
+    string arg = argv[iarg];
+    string::size_type ipos = arg.find("=");
+    if ( ipos == string::npos ) {
+      cout << "Invalid command line argument: " << arg << endl;
+      return 3;
+    }
+    string name = arg.substr(0, ipos);
+    string sval = arg.substr(ipos+1);
+    istringstream ssval(sval);
+    double val;
+    ssval >> val;
+    int ival = val + 0.49;
+    if      ( name == "x" ) { posopt = true; x0 = val; }
+    else if ( name == "y" ) { posopt = true; y0 = val; }
+    else if ( name == "z" ) { posopt = true; z0 = val; }
+    else if ( name == "t" ) { posopt = true; t0 = val; }
+    else if ( name == "posopt" ) posopt = ival;
+    else {
+      cout << "Invalid name for command line argument: " << arg << endl;
+      return 4;
+    }
+  }
+
+  if ( posopt == 0 ) {
+    cout << "Particle positions taken from particles." << endl;
+  } else if ( posopt == 1 ) {
+    cout << "Particle positions taken from vertices." << endl;
+  } else if ( posopt == 2 ) {
+    cout << "Particle positions fixed:" << endl;
+    cout << "Position option:" << endl;
+    cout << "  x0 = " << x0 << " mm " << endl;
+    cout << "  y0 = " << x0 << " mm " << endl;
+    cout << "  z0 = " << x0 << " mm " << endl;
+    cout << "  t0 = " << x0 << " mm/c" << endl;
+  }
 
   TTree* ptree = dynamic_cast <TTree *>(pfile->Get("gtree"));
   if ( ptree == nullptr ) {
@@ -68,17 +115,28 @@ int main(int argc, char** argv) {
     ptree->GetEntry(ient);
     NtpMCRecHeader evhdr = pmcrec->hdr;
     EventRecord& event = *(pmcrec->event);
-    TObjArrayIter iobj(&event);
     unsigned int npar = event.GetEntries();
+    cout << "Processing event " << evhdr.ievent << endl;
     fout << evhdr.ievent << " " << npar << endl;
     unsigned int ipar = 0;
+    TObjArrayIter iobj(&event);
+    TLorentzVector* pvtx = event.Vertex();
+    double x = x0;
+    double y = y0;
+    double z = z0;
+    double t = t0;
+    if ( posopt == 1 ) {
+      double x = pvtx->X();
+      double y = pvtx->Y();
+      double z = pvtx->Z();
+      double t = pvtx->T();
+    }
     while ( TObject* pobj = iobj.Next() ) {
       GHepParticle* ppar = dynamic_cast<GHepParticle*>(pobj);
       if ( ppar == nullptr ) {
         cout << "Unexpected invalid particle!" << endl;
         return 5;
       }
-      cout << "  Particle " << ipar << ": " << long(pobj) << endl;
       string sep = " ";
       double posfac = 1000.0;  // Convert m to mm
       double timfac = 1000.0*299792458.0;  // Convert s to mm/c.
@@ -93,10 +151,16 @@ int main(int argc, char** argv) {
       fout << sep << ppar->Pz();
       fout << sep << ppar->E();
       fout << sep << ppar->Mass();
-      fout << sep << posfac*ppar->Vx();
-      fout << sep << posfac*ppar->Vy();
-      fout << sep << posfac*ppar->Vz();
-      fout << sep << timfac*ppar->Vt();
+      if ( posopt == 0 ) {
+        x = posfac*ppar->Vx();
+        y = posfac*ppar->Vy();
+        z = posfac*ppar->Vz();
+        t = timfac*ppar->Vt();
+      }
+      fout << sep << x;
+      fout << sep << y;
+      fout << sep << z;
+      fout << sep << t;
       fout << endl;
       ++ipar;
     }
